764f37f0b43decc7459f37534642e521098fc156
HyeonSoo Kim <95853235+BrightHyeon@users.noreply.github.com>
2022-07-30 18:16:15 +0900

    initial commit

---
+++ MuhyeobSimulation/MuhyeobSimulation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MuhyeobSimulation
+//
+//  Created by Hyeonsoo Kim on 2022/07/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MuhyeobSimulation/MuhyeobSimulation/MuhyeobSimulationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MuhyeobSimulationApp.swift
+//  MuhyeobSimulation
+//
+//  Created by Hyeonsoo Kim on 2022/07/30.
+//
+
+import SwiftUI
+
+@main
+struct MuhyeobSimulationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
